20052077294b49f5d2da9405dd06545231aea33f
Manasa VK <vkmanasa2505@gmail.com>
2023-02-05 15:27:40 -0500

    Data

---
+++ AACTConfig.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  3 20:17:21 2023
+
+@author: karun
+"""
+
+Hostname= "aact-db.ctti-clinicaltrials.org"
+Port="5432"
+Databasename="aact"
+IDUsername="manasa25"
+Password="Manasasai21*"
