37f7c02d6f861aa57eb1d3a054b8fe5d49457074
Manasa VK <vkmanasa2505@gmail.com>
2023-02-05 15:27:12 -0500

    Test Code

---
+++ Sample Test Code.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 15:08:22 2023
+
+@author: karun
+"""
+
+##General Plotting (Not part of the Code just for Ref)
+
+
+### x=df_study.sample(500)
+### x["enrollment"].plot(kind="hist")
+
+
+### df_con.sample(10)
+### df_con["mesh_term"].nunique()
+### x=df_con["mesh_term"].value_counts()
+
+
+
+###location=st.sidebar.selectbox('Location',(df_facil["state"].unique()))
+
+
+
+
+##Sample merg two tables
+
+##"""df_merge=df_study.merge(df_spon,on="nct_id",how="left")"""
+
+## df_con=df_con[df_con['mesh_term']==disease]
+
